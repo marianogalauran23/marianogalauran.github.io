d4046f3d99f90b01c99215d33eb08a4666c2a819
Khiqyam <marianogalauran23@gmail.com>
2025-11-16 23:59:05 +0800

    implemented no indexing for search Engines

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
+// next.config.js
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex, nofollow', 
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
